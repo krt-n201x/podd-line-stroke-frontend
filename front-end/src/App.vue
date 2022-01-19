26e33047e0189f25747f943a24aa6c6068988256
Kitsada <kitsada_t@cmu.ac.th>
2022-01-19 21:36:40 +0700

    Install font
    Able to use "Prompt" font as a Thai font.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,38 +1,12 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-    <b-collapse id="nav-collapse" is-nav>
+  <b-navbar class="navbar navbar-light" style="background-color: #7746C3; color: white; padding-left: 24px; padding-right: 24px;">
+    <b-navbar-brand href="#"><img src="..\asset\back.png" width="30px"></b-navbar-brand>
+    <b-collapse class="d-flex justify-content-center" id="nav-text-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <b-navbar-nav>
+          <b-nav-text style="color: #ffffff; width: 100%; font-size: 24px; font-family: 'Prompt', sans-serif;" >แก้ไขข้อมูลผู้ดูแล</b-nav-text>
+        </b-navbar-nav>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -48,12 +22,23 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap');
+
+.text {
+  color: #ffffff; 
+  width: 100%; 
+  font-size: 24px; 
+  font-family: 'Prompt', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Prompt', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+  
 }
 </style>
